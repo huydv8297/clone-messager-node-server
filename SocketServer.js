7edca8f0497e7a587acf416b8d2c44f8e67dc2ad
Duong Van Huy <huydv@rikkeisoft.com>
2019-11-12 16:09:12 +0700

    commit

--- SocketServer.js
+++ SocketServer.js
@@ -11,8 +11,7 @@ class SocketServer{
             console.log("Connected successfully to socket.io")
             client.on('username', data =>{
                 this.listUserOnline.set(data.username, client)
-                console.log(data)
-                console.log(this.listUserOnline.get("huydv"))
+                console.log(this.listUserOnline.get(data.username))
             })
         })
 
