011f42f9054baf12b327b599cd7100017a05171a
Duong Van Huy <huydv@rikkeisoft.com>
2019-11-13 13:57:11 +0700

    commit

--- controller/UserController.js
+++ controller/UserController.js
@@ -13,7 +13,8 @@ class UserController {
         var fullnameReq = request.body.fullname || "default"
         var gender = request.body.gender || "Nam"
         var avatarReq = request.body.avatar || "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
-        var friendReq = request.body.friends == null ? [] : JSON.parse(friendReq)
+        var friendReq = request.body.friends == null ? [] : JSON.parse(JSON.stringify(friendReq))
+        console.log(request.body.friends)
         let user = { isExist: false }
 
         self.checkUserExist(usernameReq, user).then(() => {
