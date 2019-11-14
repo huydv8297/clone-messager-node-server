aaf0ceed7741ecc2d592513d274a3dc79f128a77
Duong Van Huy <huydv@rikkeisoft.com>
2019-11-14 09:48:28 +0700

    commit

--- SocketServer.js
+++ SocketServer.js
@@ -62,7 +62,7 @@ class SocketServer{
                     }
                 }
 
-                if(idChat == null){
+                if(idChat){
                     messageController.createNewChat(request, respone)
                 }else{
                     messageController.insertMessage(request, respone)
