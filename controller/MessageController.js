b1b3a63894a9f2c1bc8062229d7b8079cc756980
Duong Van Huy <huydv@rikkeisoft.com>
2019-11-14 11:08:16 +0700

    commit

--- controller/MessageController.js
+++ controller/MessageController.js
@@ -11,7 +11,7 @@ class MessageController {
         let idChat = request.params.idChat
         let query
         if(idChat)
-            query = {_id : idChat}
+            query = {_id : ObjectID(idChat)}
         else
             query = {}
    
