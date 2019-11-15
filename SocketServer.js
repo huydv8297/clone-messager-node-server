bf362bfa34f2f5a33b07b702f34383452104798f
Duong Van Huy <huydv@rikkeisoft.com>
2019-11-15 15:56:34 +0700

    commit

--- SocketServer.js
+++ SocketServer.js
@@ -11,9 +11,9 @@ class SocketServer{
         this.listen('connection', client => {
             console.log("Connected successfully to socket.io")
 
-            client.on('username', data =>{
-                //let data = JSON.parse(dataReq)
-                console.log(data)
+            client.on('username', dataReq =>{
+                let data = JSON.parse(dataReq)
+                console.log(dataReq)
                 console.log("add " + data.username + " to list")
                 this.listUser.set(data.username, client)
             })
